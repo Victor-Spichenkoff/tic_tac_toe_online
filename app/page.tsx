4a57e7838a8814a194e61c2269199597420f396b
Victor-Spichenkoff <victorspich13@gmail.com>
2025-01-14 21:46:57 -0300

    Go back to no src

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+// import {Chat} from "@/components/functions/Chat";
+
+export default function Home() {
+  return (
+    <div>
+      Light
+      {/* <Chat/> */}
+    </div>
+  )
+}
