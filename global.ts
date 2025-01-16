d81c1fc220ee92de5e4f815d9a2b34fceb5e7a3a
Victor-Spichenkoff <victorspich13@gmail.com>
2025-01-16 14:37:45 -0300

    Use secure socket

--- global.ts
+++ global.ts
@@ -1,3 +1,3 @@
 export const apiUrl = process.env.NODE_ENV == "development" ? "http://localhost:2006" : "https://tic-tac-toe-online-backend-jjv9.onrender.com"
 
-export const socketUrl = process.env.NODE_ENV == "development" ? "ws://localhost:2006" : "ws://tic-tac-toe-online-backend-jjv9.onrender.com"
+export const socketUrl = process.env.NODE_ENV == "development" ? "ws://localhost:2006" : "wss://tic-tac-toe-online-backend-jjv9.onrender.com"
